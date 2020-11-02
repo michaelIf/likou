2add03d98a7d65552deca20b030381f1678a37b6
jianghong <905846015@qq.com>
2020-11-02 11:06:20 +0800

    '20201102'

--- 最接近的三数之和.py
+++ 最接近的三数之和.py
@@ -1,8 +1,34 @@
 class Solution:
     def threeSumClosest(self, nums: list, target: int):
         nums.sort()
-        for i in nums:
-            pass
+        n = len(nums)
+        best = 10**7
+
+        def update(cur):
+            nonlocal best
+            if abs(cur-target) < abs(best-cur):
+                best = cur
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            j, k = i + 1, n - 1
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                if s == target:
+                    return target
+                update(s)
+                if s > target:
+                    k0 = k - 1
+                    while j < k0 and nums[k0] == nums[k]:
+                        k0 -= 1
+                    k = k0
+                else:
+                    j0 = j + 1
+                    while j0 < k and nums[j0] == nums[j]:
+                        j0 += 1
+                    j = j0
+        return best
+
 
 
 
