5d2f7f269873abada37b5a218de88b5041b3927f
jianghong <905846015@qq.com>
2020-11-04 22:07:46 +0800

    '20201104'

---
+++ 最长的回文子串.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = ""
+        print(dp)
+        for l in range(n):
+            for i in range(n):
+                j = i + l
+                if j >= len(s):
+                    break
+                if l == 0:
+                    dp[i][j] = True
+                elif l == 1:
+                    dp[i][j] = (s[i] == s[j])
+                else:
+                    dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
+                if dp[i][j] and l + 1 > len(ans):
+                    ans = s[i:j+1]
+        return ans
+
+
+test = 'aba'
+ss = Solution()
+tt = ss.longestPalindrome(test)
+print(tt)
+
+
+
+
