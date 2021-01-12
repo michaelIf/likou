9edaf441dec2158b9fbb44bb13a9983ca13ff469
jianghong <905846015@qq.com>
2021-01-12 19:38:41 +0800

    update 20210112-02

--- 链表/回文链表.py
+++ 链表/回文链表.py
@@ -1,4 +1,21 @@
 # 234
-#
-def isPalindrome(head):
-    pass
+# author official
+# 将链表转化成列表
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+class Solution:
+    def isPalindrome(self, head: ListNode):
+        vals = []
+        cur = head
+        while cur is not None:
+            vals.append(cur.val)
+            cur = cur.next
+        return vals == vals[::-1]
+
+
+
+
+ss = Solution()
