625122a84040b0c49edfc28cb979b95e12fe4299
jianghong <905846015@qq.com>
2021-01-27 20:18:27 +0800

    add 20210127

---
+++ 数组/合并两个有序数组.py
@@ -0,0 +1,26 @@
+# 88
+# official
+def merge(nums1,nums2,m,n):
+    nums1[:] = sorted(nums1[:m] + nums2)
+
+
+
+#
+def way2(nums1,nums2,m,n):
+    p1 = m - 1
+    p2 = n - 1
+    p = m + n - 1
+    while p1 >= 0 and p2 >=0:
+        if nums1[p1] < nums2[p2]:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+        else:
+            nums1[p] = nums1[p1]
+            p1 -= 1
+        p -= 1
+    # 处理nums1为空的情况
+    nums1[:p2+1] = nums2[:p2 + 1]
+    return nums1
+
+
+
